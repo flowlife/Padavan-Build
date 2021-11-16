ebb2eaaaf8c97aeefe50b509e834a19f3ae68d9b
yhsurc <83955825+yhsurc@users.noreply.github.com>
2021-11-16 14:23:59 +0800

    Update go.sh

--- bashScripts/go.sh
+++ bashScripts/go.sh
@@ -11,7 +11,7 @@ echo $i >> $goVersions
 done
 new_ver=`sed -n '1p' $goVersions`
 old_ver=`sed -n '2p' $goVersions`
-echo "rm -rf $goCacheRootDir/go/1.15.15"
-rm -rf $goCacheRootDir/go/1.15.15
-echo "cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/go/1.15.15"
-cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/go/1.15.15
+echo "rm -rf $goCacheRootDir/1.15.15"
+rm -rf $goCacheRootDir/1.15.15
+echo "cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/1.15.15"
+cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/1.15.15
